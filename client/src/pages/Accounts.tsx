2819dce64f6ae1eadfac364095ba6dea19b6dbcf
Sohaib Aslam <seboodogar@gmail.com>
2026-07-27 13:01:02 +0500

    Accounts page completed

--- client/src/pages/Accounts.tsx
+++ client/src/pages/Accounts.tsx
@@ -1,18 +1,37 @@
-import { useState } from "react"
-import { PLATFORMS } from "../assets/assets"
+import { useEffect, useState } from "react"
+import { dummyAccountsData, PLATFORMS } from "../assets/assets"
 import { PlusIcon } from "lucide-react"
 import AccountList from "../components/AccountList"
+import PlatformPickerModal from "../components/PlatformPickerModal"
 
 const Accounts = () => {
   const [accounts, setAccounts] = useState<any[]>([])
   const [connecting, setConnecting] = useState<string | null>(null)
   const [showPlatformPicker, setShowPlatformPicker] = useState(false)
 
+  const fetchAccounts = async (isSync = false, platform?: string | null, successMsg?: string) => {
+    setAccounts(dummyAccountsData);
+    console.log(isSync, platform, successMsg)
+  }
+
   const handleDisconnect = async (accountId: string) => {
-    setAccounts(accounts.filter((a) => a.id !== accountId))
+    setAccounts(accounts.filter((a) => a._id !== accountId))
   }
 
   const connectedIds = accounts.map((a) => a.platform);
+
+  const handleConnect = async (platformId: string) => {
+    setConnecting(platformId)
+    setTimeout(() =>{
+      setConnecting(null)
+      setAccounts((prev) => [...prev, dummyAccountsData[0]])
+      setShowPlatformPicker(false)
+    }, 1000)
+  }
+
+  useEffect(() => {
+    fetchAccounts()
+  }, [])
 
   return (
     <div className="space-y-8 max-w-4xl">
@@ -28,6 +47,7 @@ const Accounts = () => {
       </div>
 
       {/* Platform picker modal */}
+      {showPlatformPicker && <PlatformPickerModal connectedIds={connectedIds} connecting={connecting} onClose={() => setShowPlatformPicker(false)} onConnect={handleConnect} />}
 
       {/* Connected Accounts list */}
       <AccountList accounts={accounts} onDisconnect={handleDisconnect} />
